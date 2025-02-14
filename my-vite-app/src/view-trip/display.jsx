bc04604c245ab4a9abf8245fd19e915f7b0f51c2
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-14 09:59:27 -0600

    applying dynamic routing with explanations

--- my-vite-app/src/view-trip/display.jsx
+++ my-vite-app/src/view-trip/display.jsx
@@ -2,7 +2,14 @@ import { useParams } from "react-router-dom";
 
 function ViewTrip() {
     const { tripId } = useParams();
-    return <div>Viewing Trip ID: {tripId}</div>;
+    return <div>Viewing Trip ID: {tripId}</div>; 
 }
 
 export default ViewTrip;
+
+
+//as the dynamic route syntax did not work out directly in the folder 
+// i.e view-trip/ [tripId],
+//I created it inside the page directly over here with the trip id number mentioned 
+//it is handling and routing as needed for a new id everytime 
+ 
