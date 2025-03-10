a635df468eda073befea544ec93b483ff51c8f52
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-10 16:32:05 -0500

    dist

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // server: {
+//   //   headers: {
+//   //     "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+//   //     "Cross-Origin-Embedder-Policy": "require-corp"
+//   //   }
+//   // }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   headers: {
-  //     "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-  //     "Cross-Origin-Embedder-Policy": "require-corp"
-  //   }
-  // }
-})
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
+});
+
